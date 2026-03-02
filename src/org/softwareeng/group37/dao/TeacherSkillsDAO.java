bbc3a9ef3e4e5322e9d418ad4cbffe4fe130db31
candolp <candolpd@gmail.com>
2026-03-02 22:45:17 +0000

    Refactor `TeacherSkillsDAO` to improve formatting and import handling

--- src/org/softwareeng/group37/dao/TeacherSkillsDAO.java
+++ src/org/softwareeng/group37/dao/TeacherSkillsDAO.java
@@ -2,13 +2,15 @@ package org.softwareeng.group37.dao;
 
 import org.softwareeng.group37.model.TeacherSkills;
 
-public class TeacherSkillsDAO extends EntityDao<TeacherSkills>{
+import java.util.List;
+
+public class TeacherSkillsDAO extends EntityDao<TeacherSkills> {
     public TeacherSkillsDAO(String filename) {
         super(filename, TeacherSkills.class);
     }
 
     public TeacherSkillsDAO() {
-        super("teacherSkills.csv",TeacherSkills.class);
+        super("teacherSkills.csv", TeacherSkills.class);
     }
 
     @Override
@@ -16,3 +18,4 @@ public class TeacherSkillsDAO extends EntityDao<TeacherSkills>{
         return super.write(data);
     }
 }
+
